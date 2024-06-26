9e9498fdeada8db21fc47b25a579e89b663e40a5
Melvin_Larsson <melvin.larsson@hotmail.se>
2024-06-26 21:11:20 +0200

    Added assertion util

---
+++ utils/include/utils/assert.h
@@ -0,0 +1,21 @@
+#ifndef ASSERT_H_INCLUDED
+#define ASSERT_H_INCLUDED
+
+#include "stdio.h"
+
+#ifndef ASSERTS_ENABLED
+#define assert(valid) {}
+#else
+#define assert(valid)                                                           \
+    {                                                                           \
+        if(!(valid)){                                                           \
+            StdioColor oldColor = stdio_getColor();                             \
+            stdio_setColor(StdioColorRed);                                      \
+            printf("Assertion failed in %s at line %d\n", __FILE__, __LINE__);    \
+            stdio_setColor(oldColor);                                           \
+        }                                                                       \
+    }                                                                           
+#endif
+
+
+#endif
