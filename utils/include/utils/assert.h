1b3db1260c905004835cf93754b97db27f8ec7a3
Melvin_Larsson <melvin.larsson@hotmail.se>
2024-06-26 21:44:04 +0200

    Added condition in failed assertion print

--- utils/include/utils/assert.h
+++ utils/include/utils/assert.h
@@ -6,14 +6,14 @@
 #ifndef ASSERTS_ENABLED
 #define assert(valid) {}
 #else
-#define assert(valid)                                                           \
-    {                                                                           \
-        if(!(valid)){                                                           \
-            StdioColor oldColor = stdio_getColor();                             \
-            stdio_setColor(StdioColorRed);                                      \
-            printf("Assertion failed in %s at line %d\n", __FILE__, __LINE__);    \
-            stdio_setColor(oldColor);                                           \
-        }                                                                       \
+#define assert(valid)                                                                        \
+    {                                                                                        \
+        if(!(valid)){                                                                        \
+            StdioColor oldColor = stdio_getColor();                                          \
+            stdio_setColor(StdioColorRed);                                                   \
+            printf("Assertion failed in %s at line %d (%s)\n", __FILE__, __LINE__, #valid);  \
+            stdio_setColor(oldColor);                                                        \
+        }                                                                                    \
     }                                                                           
 #endif
 
