1008ebcaa8de7a67c69744f0a2e9a38704385a94
Melvin_Larsson <melvin.larsson@hotmail.se>
2024-08-11 10:39:10 +0200

    Load 254 sectors in booloader

--- sysroot/kernel/i686/program.s
+++ sysroot/kernel/i686/program.s
@@ -4,6 +4,10 @@ mov ss, ax
 xor ax,ax 
 mov ds, ax
 mov sp, 0xfc00
+mov si, drive_index
+mov [ds:si], dl
+jmp start
+drive_index: db 0
 %include "s1.inc"
 %include "s2.inc"
 %include "interrupt.inc"
